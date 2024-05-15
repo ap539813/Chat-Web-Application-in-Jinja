7eca4d7dd0f07a242fd63324ae37e934f75b8941
Ankush Pandey <ap539813@gmail.com>
2024-05-15 13:54:51 +0530

    Update socket_routes.py

--- socket_routes.py
+++ socket_routes.py
@@ -32,7 +32,7 @@ def connect():
     # socket automatically leaves a room on client disconnect
     # so on client connect, the room needs to be rejoined
     join_room(int(room_id))
-    emit("incoming", (f"{username} has connected", "green"), to=int(room_id))
+    emit("incoming", (f"{username} has connected", "green", "system"), to=int(room_id))
 
 # event when client disconnects
 # quite unreliable use sparingly
@@ -42,13 +42,14 @@ def disconnect():
     room_id = request.cookies.get("room_id")
     if room_id is None or username is None:
         return
-    emit("incoming", (f"{username} has disconnected", "red"), to=int(room_id))
+    emit("incoming", (f"{username} has disconnected", "red", "system"), to=int(room_id))
 
 # send message event handler
 @socketio.on("send")
 def send(username, message, room_id, rname):
     db.store_message(username, rname, message)
-    emit("incoming", (f"{username}: {message}"), to=room_id)
+    emit("incoming", (message, '', username), to=room_id)
+
     
 # join room event handler
 # sent when the user joins a room
@@ -67,18 +68,18 @@ def join(sender_name, receiver_name):
     if room_id is not None:
         room.join_room(sender_name, room_id)
         join_room(room_id)
-        emit("incoming", (f"{sender_name} has joined the room.", "green"), to=room_id, include_self=False)
-        emit("incoming", (f"{sender_name} has joined the room. Now talking to {receiver_name}.", "green"))
+        emit("incoming", (f"{sender_name} has joined the room.", "green", "system"), to=room_id, include_self=False)
+        emit("incoming", (f"{sender_name} has joined the room. Now talking to {receiver_name}.", "green", "system"))
     else:
         room_id = room.create_room(sender_name, receiver_name)
         join_room(room_id)
-        emit("incoming", (f"{sender_name} has joined the room. Now talking to {receiver_name}.", "green"), to=room_id)
+        emit("incoming", (f"{sender_name} has joined the room. Now talking to {receiver_name}.", "green", "system"), to=room_id)
 
     last_seen = db.get_last_seen(sender_name, receiver_name)
     chat_history = db.get_chat_history(sender_name, receiver_name, last_seen)
     for msg, sender in chat_history:
         color = 'blue' if sender == sender_name else 'green'
-        emit("incoming", (msg, color), to=room_id)
+        emit("incoming", (msg, color, sender), to=room_id)
     
     # Clear chat history after sending messages
     db.clear_chat_history(sender_name, receiver_name, last_seen)
@@ -88,7 +89,7 @@ def join(sender_name, receiver_name):
 # leave room event handler
 @socketio.on("leave")
 def leave(username, room_id):
-    emit("incoming", (f"{username} has left the room.", "red"), to=room_id)
+    emit("incoming", (f"{username} has left the room.", "red", "system"), to=room_id)
     leave_room(room_id)
     room.leave_room(username)
     db.update_last_seen(username, room.get_receiver(username), datetime.now())
